b53c44b29ba2b173ea2fc4a8a1394376f924bce0
guilherme.petrus <guilherme.petrus@supley.com.br>
2024-11-01 09:31:58 -0300

    publicando os arquivos da aplicacao

---
+++ saveLog.js
@@ -0,0 +1,24 @@
+const path = require('node:path');
+const fs = require('node:fs');
+
+function saveLog({ platform, arch, cpu, uptime, ramUsage, ramTotal, ramUsagePercent }) {
+    
+    const logContent = `## DETALHES DO SISTEMA
+Sitema Operacional: ${platform}
+Arquitetura do Sistema: ${arch}
+Modelo do Processador: ${cpu}
+Tempo de Atividade do Sistema: ${uptime}
+Uso de Memória RAM: ${ramUsage.toFixed(2)} GB / ${ramTotal.toFixed(2)} GB (${ramUsagePercent}%)
+------------------------------\n`;
+  
+    const logDir = path.join("/", "log")
+    
+    if (!fs.existsSync(logDir)) {
+      fs.mkdirSync(logDir)
+    }
+  
+    const logFilePath = path.join(logDir, "log.txt")
+    fs.appendFileSync(logFilePath, logContent)
+  }
+
+  module.exports = saveLog;
